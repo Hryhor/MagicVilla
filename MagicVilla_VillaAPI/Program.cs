580c8b13ce0303c26a8979930edd1fb995cb5c62
Illia <hryhorenko.illia@gmail.com>
2024-04-21 18:58:09 +0300

    Lesson 32 added new options to Model

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -4,10 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+/*Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
     .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
 
 builder.Host.UseSerilog();
+*/
 
 builder.Services.AddControllers(option =>
 {
