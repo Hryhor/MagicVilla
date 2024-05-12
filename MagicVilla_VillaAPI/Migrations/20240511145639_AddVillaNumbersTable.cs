9739d49e8273c97cd07f5c77158b2d0204f933be
Illia <hryhorenko.illia@gmail.com>
2024-05-12 14:00:31 +0300

    Lesson 57 - 58 created VillaNumberController VillaNumberRepository

---
+++ MagicVilla_VillaAPI/Migrations/20240511145639_AddVillaNumbersTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVillaNumbersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VillaNumbers",
+                columns: table => new
+                {
+                    VillaNo = table.Column<int>(type: "int", nullable: false),
+                    SpecialDetails = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VillaNumbers", x => x.VillaNo);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 5, 11, 17, 56, 39, 722, DateTimeKind.Local).AddTicks(7507));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VillaNumbers");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 4, 23, 0, 44, 32, 939, DateTimeKind.Local).AddTicks(8660));
+        }
+    }
+}
--- MagicVilla_VillaAPI/Models/Dto/VillaNumberDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaNumberDTO.cs
@@ -2,5 +2,7 @@
 {
     public class VillaNumberDTO
     {
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
     }
 }
