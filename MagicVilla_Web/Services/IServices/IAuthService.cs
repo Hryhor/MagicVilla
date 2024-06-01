7718d5dbdd12b0f4a4871e643bd5242f5152c378
Illia <hryhorenko.illia@gmail.com>
2024-06-01 15:54:02 +0300

    Part 2 lesson 42 created controller for Login and Register

--- MagicVilla_Web/Services/IServices/IAuthService.cs
+++ MagicVilla_Web/Services/IServices/IAuthService.cs
@@ -5,6 +5,6 @@ namespace MagicVilla_Web.Services.IServices
     public interface IAuthService
     {
         Task<T> LoginAsync<T>(LoginRequestDTO objToCreate);
-        Task<T> RegisterAsync<T>(UserDTO objToCreate);
+        Task<T> RegisterAsync<T>(RegistrationRequestDTO objToCreate);
     }
 }
