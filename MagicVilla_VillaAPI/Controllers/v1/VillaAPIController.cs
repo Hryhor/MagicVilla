b97a9345004df2190794a52c14ca0977995cba87
Illia <hryhorenko.illia@gmail.com>
2024-06-08 18:14:03 +0300

    Part 2 lesson 62 profile cache

--- MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -33,7 +33,7 @@ namespace MagicVilla_VillaAPI.Controllers.v1
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ResponseCache(Duration = 30)]
+        [ResponseCache(CacheProfileName = "Default30")]
         public async Task<ActionResult<APIResponse>> GetVillas()
         {
             try
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -66,6 +66,10 @@ builder.Services.AddAuthentication(x =>
 builder.Services.AddControllers(option =>
 {
     option.ReturnHttpNotAcceptable = true;
+    option.CacheProfiles.Add("Default30",
+        new CacheProfile() { 
+            Duration = 30
+        });
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
