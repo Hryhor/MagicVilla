a2739104705a9e9277192e43ee62ab215f2afbb3
Illia <hryhorenko.illia@gmail.com>
2024-06-08 14:16:51 +0300

    Part 2 lesson 55 documentaton swagger for version 1 API

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.JSInterop.Infrastructure;
 using Microsoft.OpenApi.Models;
 using Serilog;
 using System.Text;
@@ -95,6 +96,23 @@ builder.Services.AddSwaggerGen(options =>
             new List<string>()
         }
     });
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1.0",
+        Title = "Magic Villa",
+        Description = "API to manage Villa",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact =  new OpenApiContact
+        {
+            Name = "DotNet",
+            Url = new Uri("https://dotnetmastery.com"),
+        },
+       License = new OpenApiLicense
+       {
+           Name = "Example License",
+           Url = new Uri("https://examle.com/license")
+       }
+    });
 });
 
 var app = builder.Build();
@@ -103,7 +121,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Magic_VillaV1");
+    });
 }
 
 app.UseHttpsRedirection();
